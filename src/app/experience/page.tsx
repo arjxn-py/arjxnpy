e7c0792141d849d62b77842b36bd9c1c7f9118ba
arjxn.py <arjxn.py@gmail.com>
2024-01-27 20:50:26 +0530

    Configure Prettier

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -3,7 +3,7 @@ function experience() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Experience</div>
     </main>
-  )
+  );
 }
 
-export default experience
+export default experience;
