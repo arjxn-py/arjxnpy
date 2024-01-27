4995991f4c60cb44e1531b141bd691d17487e5a0
arjxn.py <arjxn.py@gmail.com>
2024-01-27 20:19:53 +0530

    Add one sample route & config title & desc

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+function experience() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Experience</div>
+    </main>
+  )
+}
+
+export default experience
