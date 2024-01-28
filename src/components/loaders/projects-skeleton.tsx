cb778f947603af337872352d2b08880f602f40dc
arjxn.py <arjxn.py@gmail.com>
2024-01-28 21:10:43 +0530

    Project card & loaders

---
+++ src/components/loaders/projects-skeleton.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function ProjectsSkeleton() {
+  return (
+    <>
+      {Array.from({ length: 6 }).map(() => (
+        <div className="flex flex-col justify-between gap-2 py-4 md:flex-row">
+          <div className="flex w-full flex-col gap-2">
+            <Skeleton className="h-5 w-[10rem] md:w-[14rem]" />
+            <Skeleton className="h-5 w-full" />
+          </div>
+          <Skeleton className="h-5 w-[3rem] md:w-0" />
+        </div>
+      ))}
+    </>
+  );
+}
---
+++ src/components/projects/project-card.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Repo } from "@/types";
+import { Star } from "lucide-react";
+
+import { CardDescription, CardTitle } from "@/components/ui/card";
+
+interface ProjectCardProps {
+  project: Repo;
+}
+
+export function ProjectCard({ project }: ProjectCardProps) {
+  return (
+    <Link
+      target="_blank"
+      href={project.link}
+      rel="noopener noreferrer"
+      aria-label={project.repo}
+      className="flex flex-col justify-between gap-2 rounded py-4 duration-100 hover:bg-muted hover:px-4 md:flex-row"
+    >
+      <div className="flex flex-col gap-1">
+        <CardTitle className="text-base">{project.repo}</CardTitle>
+        <CardDescription className="line-clamp-2 text-sm font-light">
+          {project.description}
+        </CardDescription>
+      </div>
+      <CardDescription className="flex items-center gap-1">
+        <Star className="h-4 w-4" />
+        {project.stars}
+      </CardDescription>
+    </Link>
+  );
+}
