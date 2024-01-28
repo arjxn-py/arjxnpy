bcdd9b95c38e4e36b65fdea3d331017947ea811c
arjxn.py <arjxn.py@gmail.com>
2024-01-28 13:05:42 +0530

    Add apis

---
+++ src/lib/api/github.tsx
@@ -0,0 +1,18 @@
+import wretch from "wretch";
+
+import { env } from "@/env.mjs";
+
+const apiUrl = env.GH_API_URL;
+
+// Instantiate and configure wretch
+const api = wretch(apiUrl, {
+  cache: "no-store",
+  mode: "cors",
+})
+  .errorType("json")
+  .resolve((r) => r.json());
+
+// Fetch my pinned repository
+export const getRepo = async () => {
+  return await api.get("/?username=arjxn-py");
+};
