50ddcb1cfecb301423eda4ed3b875713abe51a5f
arjxn.py <arjxn.py@gmail.com>
2024-01-28 13:32:05 +0530

    Add dashboard skeleton

---
+++ src/components/loaders/dashboard-skeleton.tsx
@@ -0,0 +1,44 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function DashboardSkeleton() {
+  return (
+    <>
+      <div className="grid w-full grid-cols-1 gap-2 md:grid-cols-2">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Coding hours</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <Skeleton className="h-8 w-[12.5rem]" />
+            <Skeleton className="h-4 w-[8rem]" />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Most used language
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <Skeleton className="h-8 w-[12.5rem]" />
+            <Skeleton className="h-4 w-[8rem]" />
+          </CardContent>
+        </Card>
+      </div>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="text-lg">Top Languages</CardTitle>
+        </CardHeader>
+        <CardContent className="grid grid-cols-2 md:grid-cols-4">
+          {Array.from({ length: 16 }).map((_, index) => (
+            <div className="space-y-2 p-3" key={index}>
+              <Skeleton className="h-4 w-[6rem]" />
+              <Skeleton className="h-4 w-[5rem]" />
+            </div>
+          ))}
+        </CardContent>
+      </Card>
+    </>
+  );
+}
