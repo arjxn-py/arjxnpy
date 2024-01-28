5941d178cf4ed4d74e8b13d4fe2d09c6828becce
arjxn.py <arjxn.py@gmail.com>
2024-01-28 11:32:21 +0530

    Initial layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,94 @@
-import type { Metadata } from "next";
+import "@/styles/globals.css";
+
+import { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Arjxn-py",
-  description: "Arjun's Portfolio",
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Radix UI",
+    "shadcn/ui",
+    "React Query",
+    "Portfolio",
+  ],
+  authors: [
+    {
+      name: siteConfig.author,
+      url: siteConfig.url,
+    },
+  ],
+  creator: siteConfig.author,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@arjxn-py",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "bg-background text-zinc-800 antialiased dark:text-zinc-200",
+          inter.className
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <div className="container flex min-h-screen flex-col py-4 md:w-[45rem] md:py-8">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
