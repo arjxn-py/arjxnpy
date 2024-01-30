29c2c10b918c32686f85580c9d4bab8af2686820
arjxn.py <arjxn.py@gmail.com>
2024-01-30 10:19:02 +0530

    Properly align content in experience-card

--- src/components/experience/experience-card.tsx
+++ src/components/experience/experience-card.tsx
@@ -11,18 +11,22 @@ export default function ExperienceCard({ experience }: ExperienceCardProps) {
       <CarouselItem>
         <div className="flex flex-col gap-1">
           <Card className="">
-            <CardTitle className="text-base pl-4 pt-4">
-              {experience.role}
-            </CardTitle>
-            <CardDescription className="line-clamp-2 text-sm font-light pl-4">
-              {experience.organization}
-            </CardDescription>
-            <CardDescription className="flex items-end gap-1 p-4 justify-end">
-              {experience.startDate} - {experience.endDate}
-            </CardDescription>
-            <CardDescription className="flex items-center gap-1 p-4">
-              {experience.location}
-            </CardDescription>
+            <div className="flex justify-between">
+              <CardTitle className="text-base pl-4 pt-4">
+                {experience.role}
+              </CardTitle>
+              <CardDescription className="flex items-end gap-1 p-4 justify-end">
+                {experience.startDate} - {experience.endDate}
+              </CardDescription>
+            </div>
+            <div className="flex justify-between">
+              <CardDescription className="line-clamp-2 text-sm font-light pl-4">
+                {experience.organization}
+              </CardDescription>
+              <CardDescription className="flex items-end gap-1 pr-4 pb-4 justify-end">
+                {experience.location}
+              </CardDescription>
+            </div>
           </Card>
         </div>
       </CarouselItem>
