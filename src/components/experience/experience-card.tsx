155916d7a143dcc0ef2a2fcee5689e6ac9893d54
arjxn.py <arjxn.py@gmail.com>
2024-04-15 17:49:03 +0530

    Configure experience card for modified object structure

--- src/components/experience/experience-card.tsx
+++ src/components/experience/experience-card.tsx
@@ -1,6 +1,7 @@
 import { Experience } from "@/types";
 import { CarouselItem } from "@/components/ui/carousel";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+
 interface ExperienceCardProps {
   experience: Experience;
 }
@@ -13,26 +14,29 @@ export default function ExperienceCard({ experience }: ExperienceCardProps) {
           <Card className="">
             <div className="flex justify-between">
               <CardTitle className="text-base pl-4 pt-4">
-                {experience.role}
+                {experience.organization.name}
               </CardTitle>
               <CardDescription className="flex items-end gap-1 p-4 justify-end">
-                {experience.startDate} - {experience.endDate}
+                {experience.organization.location}
               </CardDescription>
             </div>
             <div className="flex justify-between pb-4">
               <CardDescription className="line-clamp-2 text-sm font-light pl-4">
-                {experience.organization}
+                {experience.positions[0].role}
               </CardDescription>
               <CardDescription className="flex items-end gap-1 pr-4 pb-4 justify-end">
-                {experience.location}
+                {experience.positions[0].period}
               </CardDescription>
             </div>
-            <ul className="list-disc pl-8">
-              {experience.responsibilities.map((responsibility, index) => (
-                <li key={index} className="mb-1 pb-2">
-                  {responsibility}
-                </li>
-              ))}
+            <ul className="list-disc text-sm font-light pl-8">
+              {experience.positions[0].responsibilities &&
+                experience.positions[0].responsibilities.map(
+                  (responsibility, index) => (
+                    <li key={index} className="mb-1 pb-2">
+                      {responsibility}
+                    </li>
+                  )
+                )}
             </ul>
           </Card>
         </div>
