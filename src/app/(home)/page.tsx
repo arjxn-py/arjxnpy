de678bf708032bae96df811f953742cc8cceb088
arjxn.py <arjxn.py@gmail.com>
2024-04-16 15:27:20 +0530

    Remove project section from homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,6 @@
-import { Repo } from "@/types";
-
-import { siteConfig } from "@/config/site";
-import { getRepo } from "@/lib/api/github";
-import { HeadingText } from "@/components/common/heading-text";
 import { SocialMediaIcons } from "@/components/common/social-media-icons";
-import { ProjectCard } from "@/components/projects/project-card";
 
 export default async function Home() {
-  const data = (await getRepo()) as Repo[];
-
   return (
     <main className="py-4">
       <section className="space-y-4 py-4">
@@ -26,23 +18,6 @@ export default async function Home() {
           <span className="font-semibold">TypeScript</span>.
         </p>
         <SocialMediaIcons />
-      </section>
-      <section className="space-y-4 py-4">
-        <HeadingText>Projects</HeadingText>
-        <div className="flex flex-col items-end gap-4">
-          <div className="grid w-full grid-cols-1">
-            {data.map((project, index) => (
-              <ProjectCard key={index} project={project} />
-            ))}
-          </div>
-          <a
-            target="_blank"
-            href={`${siteConfig.links.github}?tab=repositories`}
-            className="text-sm underline"
-          >
-            See More...
-          </a>
-        </div>
       </section>
     </main>
   );
