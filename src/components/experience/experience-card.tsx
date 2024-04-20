980665d5df8c31f4e6019f54aa999a04a9939ce8
arjxn.py <arjxn.py@gmail.com>
2024-04-20 13:12:47 +0530

    Use scroll area instead of carousal

--- src/components/experience/experience-card.tsx
+++ src/components/experience/experience-card.tsx
@@ -1,5 +1,4 @@
 import { Experience } from "@/types";
-import { CarouselItem } from "@/components/ui/carousel";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 
 interface ExperienceCardProps {
@@ -9,46 +8,44 @@ interface ExperienceCardProps {
 export default function ExperienceCard({ experience }: ExperienceCardProps) {
   return (
     <>
-      <CarouselItem>
-        <div className="flex flex-col gap-1">
-          <Card className="">
-            <div className="flex justify-between">
-              <CardTitle className="text-base pl-4 pt-4">
-                {experience.organization.name}
-              </CardTitle>
-              <CardDescription className="flex items-end gap-1 p-4 justify-end">
-                {experience.organization.location}
-              </CardDescription>
-            </div>
-            {experience.positions.map((position, index) => (
-              <div key={index}>
-                <div className="flex justify-between">
-                  <CardDescription className="line-clamp-2 text-sm font-light pl-4">
-                    {position.role}
-                  </CardDescription>
-                  <CardDescription className="flex items-end gap-1 pr-4 justify-end">
-                    {position.period}
-                  </CardDescription>
-                </div>
-                <ul className="list-disc text-xs font-light pl-8">
-                  {position.responsibilities.map((responsibility, index) => (
-                    <li
-                      key={index}
-                      className={`${
-                        index === position.responsibilities.length - 1
-                          ? "mb-1 pb-2"
-                          : ""
-                      }`}
-                    >
-                      {responsibility}
-                    </li>
-                  ))}
-                </ul>
+      <div className="flex flex-col gap-1 p-1">
+        <Card className="">
+          <div className="flex justify-between">
+            <CardTitle className="text-base pl-4 pt-4">
+              {experience.organization.name}
+            </CardTitle>
+            <CardDescription className="flex items-end gap-1 p-4 justify-end">
+              {experience.organization.location}
+            </CardDescription>
+          </div>
+          {experience.positions.map((position, index) => (
+            <div key={index}>
+              <div className="flex justify-between">
+                <CardDescription className="line-clamp-2 text-sm font-light pl-4">
+                  {position.role}
+                </CardDescription>
+                <CardDescription className="flex items-end gap-1 pr-4 justify-end">
+                  {position.period}
+                </CardDescription>
               </div>
-            ))}
-          </Card>
-        </div>
-      </CarouselItem>
+              <ul className="list-disc text-xs font-light pl-8">
+                {position.responsibilities.map((responsibility, index) => (
+                  <li
+                    key={index}
+                    className={`${
+                      index === position.responsibilities.length - 1
+                        ? "mb-1 pb-2"
+                        : ""
+                    }`}
+                  >
+                    {responsibility}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </Card>
+      </div>
     </>
   );
 }
