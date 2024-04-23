5c1ebac837380a2fd533f23bff741a0a6a8985ad
arjxn.py <arjxn.py@gmail.com>
2024-04-24 00:45:04 +0530

    Start to structure form

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,58 @@
-export default async function Portfolio() {
-  return <div>Portfolio Creator</div>;
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  emailAddress: z.string().email(),
+});
+
+export default function Portfolio() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log({ values });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="emailAddress"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email Address</FormLabel>
+              <FormControl>
+                <Input placeholder="Your Email" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display Email.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
