81e45245116bd56ca29fd28391f5b732b46eb872
arjxn.py <arjxn.py@gmail.com>
2024-07-14 17:14:53 +0530

    Second person content

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,10 +6,9 @@ export default async function Home() {
       <section className="space-y-4 py-4">
         <p className="font-light text-muted-foreground">Welcome to /arjxn-py</p>
         <p className="font-light">
-          I am a junior undergrad at the University of Delhi in New Delhi,
-          India, pursuing my Bachelors of Technology degree with a major in
-          Information Technology and Mathematical Innovations with a minor in
-          Management.
+          Arjun Verma is a senior undergrad at the University of Delhi, India,
+          pursuing Bachelors of Technology with a major in Information
+          Technology and Mathematical Innovations with a minor in Management.
         </p>
         <p className="font-light">
           My main tech stack for building apps is{" "}
